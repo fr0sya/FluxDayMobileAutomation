14a2f07d81baa706d52f54a04bb58fb8f1fb729e
fr0sya <yaroksana@gmail.com>
2018-12-03 20:39:24 +0200

    first iteration

---
+++ FluxDayMobileAutomation/PageObjects/MainMenuPageObject.cs
@@ -0,0 +1,134 @@
+﻿using OpenQA.Selenium;
+
+namespace FluxDayMobileAutomation.PageObjects
+{
+    public class MainMenuPageObject
+    {
+        private const string MAIN_MENU_LINK_CSS = "body > div.fixed > nav > ul > li.toggle-topbar.menu-icon > a";
+        private const string TOB_BAR_CLASS = "top-bar expanded fixed";
+        private const string ADD_TASK_CSS = "body > div:nth-child(1) > nav > section > ul.right > li > a";
+        private const string MY_TASKS_CSS = "body > div:nth-child(1) > nav > section > ul.right > div > li:nth-child(1) > a";
+        private const string DEPARTMENTS_CSS = "body > div:nth-child(1) > nav > section > ul.right > div > li:nth-child(2) > a";
+        private const string TEAM_CSS = "body > div:nth-child(1) > nav > section > ul.right > div > li:nth-child(3) > a";
+        private const string USERS_CSS = "body > div:nth-child(1) > nav > section > ul.right > div > li:nth-child(4) > a";
+        private const string OKR_CSS = "body > div:nth-child(1) > nav > section > ul.right > div > li:nth-child(5) > a";
+        private const string REPORTS_CSS = "body > div:nth-child(1) > nav > section > ul.right > div > li:nth-child(6) > a";
+        private const string OAUTH_APPLICATIONS_CSS = "body > div:nth-child(1) > nav > section > ul.right > div > li:nth-child(7) > a";
+        private const string MANAGER_NAME_CSS = "body > div:nth-child(1) > nav > section > ul.right > div > li:nth-child(8) > a";
+        private const string EMPLOYEE_NAME_CSS = "body > div:nth-child(1) > nav > section > ul.right > div > li:nth-child(7) > a";
+        private const string MANAGER_LOGOUT_CSS = "body > div:nth-child(1) > nav > section > ul.right > div > li:nth-child(9) > a";
+        private const string EMPLOYEE_LOGOUT_CSS = "/html/body/div[1]/nav/section/ul[1]/div/li[8]/a";
+
+        private IWebDriver driver;
+
+        public MainMenuPageObject(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        public IWebElement MainMenu
+        {
+            get
+            {
+                return driver.FindElement(By.CssSelector(MAIN_MENU_LINK_CSS));
+            }
+
+        }
+
+        public IWebElement AddTask
+        {
+            get
+            {
+                return driver.FindElement(By.CssSelector(ADD_TASK_CSS));
+            }
+        }
+
+        public IWebElement MyTasks
+        {
+            get
+            {
+                return driver.FindElement(By.CssSelector(MY_TASKS_CSS));
+            }
+        }
+
+        public IWebElement DepartmentsItem
+        {
+            get
+            {
+                return driver.FindElement(By.CssSelector(DEPARTMENTS_CSS));
+            }
+        }
+
+        public IWebElement TeamItem
+        {
+            get
+            {
+                return driver.FindElement(By.CssSelector(TEAM_CSS));
+            }
+        }
+
+        public IWebElement UsersItem
+        {
+            get
+            {
+                return driver.FindElement(By.CssSelector(USERS_CSS));
+            }
+        }
+
+        public IWebElement OKRItem
+        {
+            get
+            {
+                return driver.FindElement(By.CssSelector(OKR_CSS));
+            }
+        }
+
+        public IWebElement ReportsItem
+        {
+            get
+            {
+                return driver.FindElement(By.CssSelector(REPORTS_CSS));
+            }
+        }
+
+        public IWebElement OauthApplicationItem
+        {
+            get
+            {
+                return driver.FindElement(By.CssSelector(OAUTH_APPLICATIONS_CSS));
+            }
+        }
+
+        public IWebElement ManagerNameItem
+        {
+            get
+            {
+                return driver.FindElement(By.CssSelector(MANAGER_NAME_CSS));
+            }
+        }
+
+        public IWebElement EmployeeNameItem
+        {
+            get
+            {
+                return driver.FindElement(By.CssSelector(EMPLOYEE_NAME_CSS));
+            }
+        }
+
+        public IWebElement ManagerLogoutItem
+        {
+            get
+            {
+                return driver.FindElement(By.CssSelector(MANAGER_LOGOUT_CSS));
+            }
+        }
+
+        public IWebElement EmployeeLogoutItem
+        {
+            get
+            {
+                return driver.FindElement(By.XPath(EMPLOYEE_LOGOUT_CSS));
+            }
+        }
+    }
+}
---
+++ FluxDayMobileAutomation/UITests/UnitTest1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Enums;
+using OpenQA.Selenium.Appium.Android;
+using OpenQA.Selenium.Remote;
+using FluxDayMobileAutomation.PageObjects;
+
+namespace Appium
+{
+    [TestFixture()]
+    public class AppiumTest
+    {
+        private IWebDriver driver;
+        private TimeSpan IMPLICIT_TIMEOUT_SEC = TimeSpan.FromSeconds(10);
+        private const string LOGOUT_TEXT = "Logout";
+
+        [SetUp]
+        public void BeforeAll()
+        {
+            //Set the capabilities
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            capabilities.SetCapability(MobileCapabilityType.BrowserName, "Chrome");
+            capabilities.SetCapability(MobileCapabilityType.PlatformName, "Android");
+            capabilities.SetCapability(MobileCapabilityType.PlatformVersion, "7.1.1");
+            capabilities.SetCapability(MobileCapabilityType.AutomationName, "UIAutomator2");
+            capabilities.SetCapability(MobileCapabilityType.DeviceName, "cool_c1");
+
+            driver = new AndroidDriver<AppiumWebElement>(new Uri("http://localhost:4723/wd/hub"), capabilities, TimeSpan.FromSeconds(180));
+            driver.Manage().Timeouts().ImplicitlyWait(IMPLICIT_TIMEOUT_SEC);
+            driver.Url = "https://app.fluxday.io/users/sign_in";
+        }
+
+        [TearDown]
+        public void AfterAll()
+        {
+            driver.Quit();
+        }
+
+        [Test]
+        [TestCase("admin@fluxday.io", "password", "Admin User")]
+        [TestCase("emp1@fluxday.io", "password", "Employee 1")]
+        public void LoginTest(string email, string password, string userName)
+        {
+            var login = new LoginPageObject(driver);
+            var mainManuPageObject = new MainMenuPageObject(driver);
+
+            // user email
+            login.UserEmailTextBox.Click();
+            login.UserEmailTextBox.Clear();
+            login.UserEmailTextBox.SendKeys(email);
+
+            // user password
+            login.UserPasswordTextBox.Click();
+            login.UserPasswordTextBox.Clear();
+            login.UserPasswordTextBox.SendKeys(password);
+
+            // press button
+            login.LoginButton.Click();
+
+            // open/click on main manu
+            mainManuPageObject.MainMenu.Click();
+
+            // check the expected result
+            if (userName == "Admin User")
+            {
+                Assert.AreEqual(userName, mainManuPageObject.ManagerNameItem.Text);
+                Assert.AreEqual(LOGOUT_TEXT, mainManuPageObject.ManagerLogoutItem.Text);
+            }
+            else
+            {
+                Assert.AreEqual(userName, mainManuPageObject.EmployeeNameItem.Text);
+                Assert.AreEqual(LOGOUT_TEXT, mainManuPageObject.EmployeeLogoutItem.Text);
+            }
+        }
+    }
+}
